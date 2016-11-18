e6c21ba9e704cfdb7110d1e610aec6c14c22a059
App Academy Student <student@appacademy.io>
2016-11-18 15:25:08 -0800

    Added structure and messages for turn of minesweeper.

--- minesweeper.rb
+++ minesweeper.rb
@@ -2,8 +2,10 @@
 
 class Minesweeper
   attr_reader :grid, :rows, :cols, :bombs
+  attr_accessor :game_over
 
   def initialize
+    @game_over = false
     @rows = 9
     @cols = 9
     @grid = Array.new(@rows) { Array.new(@cols) }
@@ -36,6 +38,49 @@ class Minesweeper
   def empty?(pos)
     grid[pos[0]][pos[1]].nil?
   end
+
+  def run
+    until game_over
+      ##Render board
+      play_turn
+    end
+
+    ##Print end of game message
+  end
+
+  def play_turn
+    valid_input = false
+    until valid_input
+      puts "Choose 'Reveal' or 'Flag'"
+      input = gets.chomp
+      case input
+      when 'Reveal'
+        valid_input = true
+        puts "Choose a square to reveal."
+        position = get_position
+        reveal(position)
+      when 'Flag'
+        valid_input = true
+        puts "Chosse a square to flag."
+        position = get_position
+        flag(position)
+      else
+        puts "That's not a valid choice."
+      end
+    end
+  end
+
+  def get_position
+    ##Translates an input into a position array
+  end
+
+  def reveal(pos)
+    ##Reveals square indicated by pos
+  end
+
+  def flag(pos)
+    ##Flags square indicated by pos
+  end
 end
 
 # test = Minesweeper.new
@@ -43,3 +88,4 @@ end
 # p test.grid
 #
 # p Minesweeper.new.grid
+Minesweeper.new.run
