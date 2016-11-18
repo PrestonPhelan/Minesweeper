6ff846e54fd2d2d70a7b7e1491056a4348929450
App Academy Student <student@appacademy.io>
2016-11-18 15:16:11 -0800

    Implemented board set up in initialize

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,12 +1,45 @@
+#require 'byebug'
+
 class Minesweeper
+  attr_reader :grid, :rows, :cols, :bombs
 
   def initialize
-    @grid = Array.new(9) { Array.new(9) }
-
+    @rows = 9
+    @cols = 9
+    @grid = Array.new(@rows) { Array.new(@cols) }
+    @bombs = 10
+    seed_bombs
   end
 
   def seed_bombs
-    
+    bombs.times do
+      position = []
+      loop do
+        position = [rand(@rows), rand(@cols)]
+        break if empty?(position)
+      end
+
+      self[position] = :B
+    end
   end
 
+  def [](pos)
+    row, col = pos[0], pos[1]
+    grid[row][col]
+  end
+
+  def []=(pos, value)
+    row, col = pos[0], pos[1]
+    @grid[row][col] = value
+  end
+
+  def empty?(pos)
+    grid[pos[0]][pos[1]].nil?
+  end
 end
+
+# test = Minesweeper.new
+# test[[0, 1]] = :B
+# p test.grid
+#
+# p Minesweeper.new.grid
